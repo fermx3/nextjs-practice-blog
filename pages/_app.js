0dd9b764c7a9ff699e4dd6fa057a30ddcd5837f1
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2023-02-14 18:53:08 -0600

    Add meta theme-color

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta charSet='utf-8' />
+        <meta name='theme-color' content='#221f23' />
       </Head>
       <NotificationContextProvider>
         <Component {...pageProps} />
